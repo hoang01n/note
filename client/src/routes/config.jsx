b2e441f76c441799fecc6dc62b698b737159122e
hoang01n <hoang01n@gmail.com>
2024-09-30 21:57:45 +0700

    notfound 404

--- client/src/routes/config.jsx
+++ client/src/routes/config.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import  Home  from "../pages/Home";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
 
 
- const routersConfig = [
+const routersConfig = [
 
     {
-        path:"/",
-       element: <Home/>
+        path: "/",
+        element: <Home />
     },
     {
-        path:"/login",
-       element: <Login/>
+        path: "/login",
+        element: <Login />
     },
     {
-        path:"/register",
-       element: <Register/>
+        path: "/register",
+        element: <Register />
 
-        }
+    },
+    {
+        path: "/*",
+        element: <NotFound />
+
+    }
 ];
 export default routersConfig;
